3362d400bbb23d5c47c1f2207468d967231e083b
oleg-zagurak <zagurak@gmail.com>
2023-05-23 16:51:14 +0300

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './static/header/header.component';
+import { FooterComponent } from './static/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
